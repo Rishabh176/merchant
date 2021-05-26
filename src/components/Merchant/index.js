40241a221bc7f9770c100a23078c3710db00235a
Rishabh Jain <rishabh.jain@mckinleyrice.co>
2021-05-26 18:46:00 +0530

    merchant bid table

--- src/components/Merchant/index.js
+++ src/components/Merchant/index.js
@@ -1,4 +1,6 @@
 import React, { memo, useEffect, useState } from 'react';
+import { Box, Paper, Table, TableHead, TableBody, TableCell, TableRow, Chip, Typography } from '@material-ui/core';
+import { Star } from '@material-ui/icons';
 import axios from 'axios';
 import { withRouter } from "react-router-dom";
 import { BASE_URL } from '../../utils/constant';
@@ -31,15 +33,61 @@ const Merchant = memo((props) => {
     }
 
     console.log("data--->", merchantData);
+    const { bids } = merchantData;
+    console.log("bids--->", bids);
 
     if (loading) {
         return <Loader />;
     }
     else {
         return (
-            <div>
-                Merchant Page      
-            </div>
+            <Box>
+                <Paper>
+                    <Box>
+                        <img src={merchantData?.avatarUrl} />
+                    </Box>
+                    <Box>
+                        {
+                            merchantData?.hasPremium === true ? 
+                            <Chip 
+                                size="small"
+                                icon={<Star />}
+                                label="Premium"
+                                color="secondary"
+                            /> : null
+                        }
+                        <Typography>{merchantData?.firstname + ' ' + merchantData?.lastname}</Typography>
+                        <Typography>{merchantData?.email}</Typography>
+                        <Typography>{merchantData?.phone}</Typography>
+                    </Box>             
+                </Paper>
+                <Paper>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Car</TableCell>
+                                <TableCell>Amount</TableCell>
+                                <TableCell>Created</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                bids.length > 0 && bids.map((bid) => {
+                                    var date = new Date(bid.created * 1000); 
+                                    var formattedTime = date.toLocaleString();
+                                    return (
+                                        <TableRow key={bid.id}>
+                                            <TableCell>{bid.carTitle}</TableCell>
+                                            <TableCell>{bid.amount}</TableCell>
+                                            <TableCell>{formattedTime}</TableCell>
+                                        </TableRow>
+                                    );
+                                }) 
+                            }
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </Box>
         );
     }
 })
